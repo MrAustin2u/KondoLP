9dafd8acebd61ef75cc523565bdaf55e94d20c81
MrAustin2u <aaustin86@gmail.com>
2020-04-09 17:02:12 -0700

    Due to Landing page not needing state management the team decided to write landing page with vanilla javascript, html/css/scss

--- client/LandingPage.jsx
+++ client/LandingPage.jsx
@@ -1,45 +1,45 @@
-import React from "react";
-import ReactFullpage from "@fullpage/react-fullpage";
+// import React from "react";
+// import ReactFullpage from "@fullpage/react-fullpage";
 
-const fullPageOptions = {
-  autoScrolling: true,
-  scrollingSpeed: 1000,
-  anchors: ["section1", "section2", "section3"],
-  lockAnchors: true,
-  navigation: true,
-  //   navigationPosition: "right",
-  navigationTooltips: ["Kondo", "Installation", "About Us"],
-  scrollOverFlow: true,
-};
+// const fullPageOptions = {
+//   autoScrolling: true,
+//   scrollingSpeed: 1000,
+//   anchors: ["section1", "section2", "section3"],
+//   lockAnchors: true,
+//   navigation: true,
+//   //   navigationPosition: "right",
+//   navigationTooltips: ["Kondo", "Installation", "About Us"],
+//   scrollOverFlow: true,
+// };
 
-const LandingPage = (fullpageProps) => (
-  <ReactFullpage
-    {...fullpageProps}
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper {...fullPageOptions}>
-          <div className="section" id="section1">
-            <h1 className="main-title">KONDO</h1>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Installation
-            </button>
-          </div>
-          <div className="section" id="section2">
-            <h1 className="install">Installation</h1>
-            <a href="/kondo">Read More</a>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              About Us
-            </button>
-          </div>
-          <div className="section" id="section3">
-            <h1 className="aboutUs">About Us</h1>
-            <button onClick={() => fullpageApi.moveSectionUp()}>
-              Installation
-            </button>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
-);
-export default LandingPage;
+// const LandingPage = (fullpageProps) => (
+//   <ReactFullpage
+//     {...fullpageProps}
+//     render={({ state, fullpageApi }) => {
+//       return (
+//         <ReactFullpage.Wrapper {...fullPageOptions}>
+//           <div className="section" id="section1">
+//             <h1 className="main-title">KONDO</h1>
+//             <button onClick={() => fullpageApi.moveSectionDown()}>
+//               Installation
+//             </button>
+//           </div>
+//           <div className="section" id="section2">
+//             <h1 className="install">Installation</h1>
+//             <a href="/kondo">Read More</a>
+//             <button onClick={() => fullpageApi.moveSectionDown()}>
+//               About Us
+//             </button>
+//           </div>
+//           <div className="section" id="section3">
+//             <h1 className="aboutUs">About Us</h1>
+//             <button onClick={() => fullpageApi.moveSectionUp()}>
+//               Installation
+//             </button>
+//           </div>
+//         </ReactFullpage.Wrapper>
+//       );
+//     }}
+//   />
+// );
+// export default LandingPage;
